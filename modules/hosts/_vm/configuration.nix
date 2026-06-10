1f91531356ea336995cc1233cb97de4e17d8a633
SibaldH <sibald.hulselmans@protonmail.com>
2026-06-10 14:21:08 +0200

    feat: Add new host vm

---
+++ modules/hosts/_vm/configuration.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }: {
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    loader.grub.enable = true;
+    loader.grub.efiSupport = true;
+    loader.grub.device = "nodev";
+    loader.grub.useOSProber = true;
+    loader.efi.canTouchEfiVariables = true;
+  };
+
+  nixconf = {
+    user = "jay";
+    fullName = "Sibald Hulsemlans";
+    email = "sibald.hulselmans@protonmail.com";
+    hostName = "vm";
+  };
+
+}
---
+++ modules/hosts/vm.nix
@@ -0,0 +1,28 @@
+{
+  self,
+  inputs,
+  withSystem,
+  ...
+}:
+{
+  flake.nixosConfigurations.vm = withSystem "x86_64-linux" (
+    { self', inputs', ... }:
+    inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit
+          inputs
+          inputs'
+          self
+          self'
+          ;
+      };
+
+      modules = [
+        self.nixosModules.profile-workstation
+
+        ./_vm/hardware.nix
+        ./_vm/configuration.nix
+      ];
+    }
+  );
+}
