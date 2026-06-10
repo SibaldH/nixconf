d3938733bacf813a833e82d9c233cdb731dd7b86
SibaldH <sibald.hulselmans@protonmail.com>
2026-06-10 16:27:16 +0200

    Update minimal.nix

--- modules/profiles/minimal.nix
+++ modules/profiles/minimal.nix
@@ -8,12 +8,9 @@
         self.nixosModules.base-locales
         self.nixosModules.base-network
         self.nixosModules.base-nix
-        self.nixosModules.base-plymouth
+        # self.nixosModules.base-plymouth
         self.nixosModules.base-settings
         self.nixosModules.base-users
-
-        # services
-        self.nixosModules.services-greetd
       ];
 
       environment.systemPackages = with pkgs; [
