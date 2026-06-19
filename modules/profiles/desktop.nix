902a9567735952c55ee239afc5b3bc24e53babf1
SibaldH <sibald.hulselmans@protonmail.com>
2026-06-19 22:20:14 +0200

    fix: Allow all systems again

--- modules/profiles/desktop.nix
+++ modules/profiles/desktop.nix
@@ -7,13 +7,13 @@
       imports = [
         self.nixosModules.profile-cli
 
-        # self.nixosModules.hardware-audio
-        # self.nixosModules.hardware-bluetooth
-        # self.nixosModules.hardware-graphics
+        self.nixosModules.hardware-audio
+        self.nixosModules.hardware-bluetooth
+        self.nixosModules.hardware-graphics
 
-        # self.nixosModules.desktop-fonts
-        # self.nixosModules.desktop-niri
-        # self.nixosModules.desktop-xdg
+        self.nixosModules.desktop-fonts
+        self.nixosModules.desktop-niri
+        self.nixosModules.desktop-xdg
 
         # services
         self.nixosModules.services-greetd
