e51a426bb48a359d27efe39712a7ab931a0ffeba
SibaldH <sibald.hulselmans@protonmail.com>
2026-06-19 21:00:28 +0200

    feat: jay as trusted user

--- modules/hosts/_vm/configuration.nix
+++ modules/hosts/_vm/configuration.nix
@@ -10,6 +10,10 @@
 
   services.qemuGuest.enable = true;
 
+  nix.settings.trusted-users = [
+    "root"
+    "jay"
+  ];
   security.sudo.wheelNeedsPassword = false;
   users.users.${config.nixconf.user}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOsiIMWKFoaBLwckqq0BpbWRXYsctBxchdvbbBLbVIIx jay@nixconf-vm"
