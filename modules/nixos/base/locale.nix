63d15362a8ddd888a4947c32dfffda15192bb180
SibaldH <sibald.hulselmans@protonmail.com>
2026-07-31 14:57:19 +0200

    change: locales, timezone and console keyMap

--- modules/nixos/base/locale.nix
+++ modules/nixos/base/locale.nix
@@ -2,21 +2,21 @@
 
 {
   flake.nixosModules.base-locales = { pkgs, lib, ... }: {
-    i18n = {
-      defaultLocale = "en_US.UTF-8";
-      extraLocaleSettings = {
-        LANGUAGE = "en_US.UTF-8";
-        LANG = "en_US.UTF-8";
-        LC_TIME = "nl_BE.UTF-8";
-        LC_NUMERIC = "nl_BE.UTF-8";
-        LC_IDENTIFICATION = "nl_BE.UTF-8";
-        LC_MONETARY = "nl_BE.UTF-8";
-        LC_ADDRESS = "nl_BE.UTF-8";
-        LC_TELEPHONE = "nl_BE.UTF-8";
-        LC_MEASUREMENT = "nl_BE.UTF-8";
-        LC_NAME = "nl_BE.UTF-8";
-        LC_MESSAGES = "en_US.UTF-8";
-      };
+    time.timeZone = "Europe/Brussels";
+
+    console.keyMap = "be-latin1";
+
+    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.extraLocaleSettings = {
+	  LC_ADDRESS = "nl_BE.UTF-8";
+	  LC_IDENTIFICATION = "nl_BE.UTF-8";
+	  LC_MEASUREMENT = "nl_BE.UTF-8";
+	  LC_MONETARY = "nl_BE.UTF-8";
+	  LC_NAME = "nl_BE.UTF-8";
+	  LC_NUMERIC = "nl_BE.UTF-8";
+	  LC_PAPER = "nl_BE.UTF-8";
+	  LC_TELEPHONE = "nl_BE.UTF-8";
+	  LC_TIME = "nl_BE.UTF-8";
+	};
     };
-  };
 }
