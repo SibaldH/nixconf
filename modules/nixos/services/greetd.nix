8be4c8cf181df213cb1ba39baf516fdce6cef211
SibaldH <sibald.hulselmans@protonmail.com>
2026-08-19 11:49:51 +0200

    change(greetd): enable gnome keyring

--- modules/nixos/services/greetd.nix
+++ modules/nixos/services/greetd.nix
@@ -5,13 +5,18 @@
       config,
       ...
     }:
+    let
+      inherit (config.nixconf) user;
+    in
     {
       services.greetd = {
         enable = true;
         settings.default_session = {
           command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd ${config.programs.niri.package}/bin/niri-session";
-	  user = "${config.nixconf.user}";
+          user = "${user}";
         };
       };
+
+      security.pam.services.greetd.enableGnomeKeyring = true;
     };
 }
